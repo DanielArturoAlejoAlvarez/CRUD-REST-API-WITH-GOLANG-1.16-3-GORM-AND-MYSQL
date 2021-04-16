2b213743913ee52e224d6468bdaac3a2c652008a
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-16 18:37:15 -0500

    Adding UpdateUser methods to controller

--- user.go
+++ user.go
@@ -31,5 +31,11 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.First(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	DB.Save(&user)
+	json.NewEncoder(w).Encode(user)
 }
