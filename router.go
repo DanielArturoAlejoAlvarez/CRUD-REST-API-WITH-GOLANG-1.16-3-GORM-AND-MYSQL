a309e216dd8bf3ab8d7242f7184f5508c5d27e1d
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-16 18:45:36 -0500

    Config our port

--- router.go
+++ router.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func initializeRouter() {
 	r := mux.NewRouter()
@@ -10,4 +15,6 @@ func initializeRouter() {
 	r.HandleFunc("api/users", CreateUser).Methods("POST")
 	r.HandleFunc("api/users/{id}", UpdateUser).Methods("PUT")
 	r.HandleFunc("api/users/{id}", DeleteUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
