15129e2f1358b40e682e59c27a8dc8dff0cd4228
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-16 14:30:30 -0500

    Configure our User interface

--- types.go
+++ types.go
@@ -1,4 +1,15 @@
 package main
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
+	DisplayName string `json:"displayname"`
+	Email       string `json:"email"`
+	Username    string `json:"username"`
+	Phone       string `json:"phone"`
+	Address     string `json:"address"`
+	Age         int    `json:"age"`
+	Avatar      string `json:"avatar"`
+	Flag        bool   `json:"flag"`
 }
