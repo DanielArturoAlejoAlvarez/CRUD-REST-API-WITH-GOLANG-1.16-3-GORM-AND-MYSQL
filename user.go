4aef683d321534967a1b1c8148691cbf875ded81
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-16 18:16:53 -0500

    Adding GetUsers methods to controller

--- user.go
+++ user.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	DB.Find(&users)
+	json.NewEncoder(w).Encode(users)
 }
