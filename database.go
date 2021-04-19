8b5378b74c9e88bb1d9e47b47c7833e975801efe
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-04-19 13:00:43 -0500

    Refactoring our connection to DB

--- database.go
+++ database.go
@@ -11,10 +11,8 @@ var DB *gorm.DB
 
 var err error
 
-var DNS = "root:password@tcp(127.0.0.1:3306)/crud_rest_api_golang_db?charset=utf8mb4&parseTime=True&loc=Local"
-
 func InitialMigration() {
-	DB, err = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(ConnectionString), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("Can't connect to DB!")
